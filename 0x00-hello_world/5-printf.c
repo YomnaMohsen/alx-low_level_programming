058568a0976fb0f4b604eba8de4eee2506af0f6e
YomnaMohsen <ymn_mohsen@yahoo.com>
2023-09-07 12:11:52 -0700

    task to use printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - display  with proper grammar, but the outcome is a piece of art
+ * Return: main return 0 and exit the program
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

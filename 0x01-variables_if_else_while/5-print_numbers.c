95c334a5a41cff4b08c052c1238304d9cfb93058
YomnaMohsen <ymn_mohsen@yahoo.com>
2023-09-08 11:28:43 -0700

    display single digit of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - display lower case alphabet
+ * Return: 0 and exit the program.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}

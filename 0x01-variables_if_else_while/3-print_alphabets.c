2c0520ef73a8812cd334450f79837dc70eef84ae
YomnaMohsen <ymn_mohsen@yahoo.com>
2023-09-08 11:11:53 -0700

    display lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - display lower  and upper case alphabet
+ * Return: 0 and exit the program.
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}

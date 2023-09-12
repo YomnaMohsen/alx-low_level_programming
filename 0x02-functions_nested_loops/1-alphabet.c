8ac8d9163c03198d0b4a8570d15212e9d234999c
YomnaMohsen <ymn_mohsen@yahoo.com>
2023-09-12 11:58:53 -0700

    print lowecase alphabet using _putchar()

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * main- calling _putchar fn
+ *
+ * Return: 0 and exits program
+ */
+
+int  main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+	return (0);
+}

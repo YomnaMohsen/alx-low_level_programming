9ab934118ff2e471f9397a6dcacccc579549e81f
YomnaMohsen <ymn_mohsen@yahoo.com>
2023-09-12 11:40:40 -0700

    printing _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,15 +2,17 @@
 
 
 /**
- *main- calling _putchar fn
+ *
+ * main- calling _putchar fn
  * Return: 0 and exits program
  */
 
 int  main(void)
 {
 	char msg[] = "_putchar";
+	int i;
 
-	for (int i = 0; i < 9; i++)
+	for (i = 0; i < 9; i++)
 		_putchar(msg[i]);
 	_putchar('\n');
 	return (0);

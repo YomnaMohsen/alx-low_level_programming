fa3cad8888920d3d4a9edb8f934efa01e8ad6f5b
YomnaMohsen <ymn_mohsen@yahoo.com>
2023-09-13 13:40:28 -0700

    function that adds 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+ *add- add 2 digit numbers
+ *
+ * @x: first number that will be added
+ * @y: second number that will be added
+ * Return: result of addition
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}

719f9ed167c9f767b2b526f400c2a3862b349b0f
YomnaMohsen <ymn_mohsen@yahoo.com>
2023-09-14 04:24:22 -0700

    print form 0 to 14 10 times using _putchar

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * more_numbers- uses _putchar to print frm 0 to 14  10 times
+ *
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(i / 10 + '0');
+			if (i / 10 > 0)
+				_putchar(i % 10 + '0');
+		}
+	}
+	_putchar('\n');
+}

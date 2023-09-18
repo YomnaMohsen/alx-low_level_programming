c532a464ec3940038ac8ea8edd98b1e028680841
YomnaMohsen <ymn_mohsen@yahoo.com>
2023-09-18 09:20:23 -0700

    find length of string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
 /* this prototype has to be declared in a header file */
 int _putchar(char c);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);

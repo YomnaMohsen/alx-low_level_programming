afac5bc57ebc31e1eafdad555ee4ba0b766d65e5
YomnaMohsen <ymn_mohsen@yahoo.com>
2023-09-20 10:38:53 -0700

    convert to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *string_toupper - convert lower case to upper
+ *
+ *@a: pointer array of char
+ * Return: Nothing
+ */
+
+char *string_toupper(char *a)
+{
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		if (a[i] < 'A' && a[i] > 'Z')
+			if (a[i] < 'a' && a[i] > 'z')
+			{
+				i++;
+				continue;
+			}
+			else
+			{
+				a[i] -= 32;
+			}
+		i++;
+	}
+	return (a);
+}
+

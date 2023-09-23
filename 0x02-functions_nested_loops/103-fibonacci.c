24aa1bc52e65ca73ec6ca8371b980c418f4e8e0d
YomnaMohsen <ymn_mohsen@yahoo.com>
2023-09-23 11:32:43 -0700

    find sum of even valued number in fib. series less than 4000000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * main- print um of even valued number in fibonacci series
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+	int  tmp1 = 1, tmp2 = 1, series;
+
+	while (series < 4000000)
+	{
+		series = tmp1 + tmp2;
+		tmp1 = tmp2;
+		tmp2 = series;
+		if (tmp2 % 2 == 0)
+		{
+			sum += tmp2;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}

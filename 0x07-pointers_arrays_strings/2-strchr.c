fb9ce9bbf6bb1aaabf840c0e99122dc928b8a5e7
YomnaMohsen <ymn_mohsen@yahoo.com>
2023-09-25 07:44:59 -0700

    find 1st occuerence of char in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr -  locates 1st occuerence of charcter in string
+ * @c: charcter to search for
+ * @s: pointer to source mem
+ * Return: pointer to string
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0, found = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			found = 1;
+			break;
+		}
+		s++;
+	}
+	if (found)
+	{
+		return (s);
+	}
+	
+	else
+		return (NULL);
+}

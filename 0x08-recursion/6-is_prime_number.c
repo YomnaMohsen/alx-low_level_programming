39b6a51dd6dcb5efc033688880b9f44f605841d8
YomnaMohsen <ymn_mohsen@yahoo.com>
2023-09-28 13:50:48 -0700

    check if numer is prime or not

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*find_prime: helps to check whether number passed to is_prime_number
+*@x: int number to be checked
+*@n: int number that x will be divided by
+*Return: 1 if number is prime, 0 otherwise
+*/
+int find_prime(int x, int n)
+{
+	if (x == n)
+	{
+		return (1);
+	}
+	if (x % n == 0)
+		return (0);
+	find_prime(x, n + 1);
+
+}
+
+/**
+ * is_prime_number - checks if int number is prime or not
+ * @x: int number
+ * Return: return 1 if number is prime else return 0
+ */
+int is_prime_number(int x)
+{
+	int n = 1;
+
+	if (x == 1)
+	{
+		return (1);
+	}
+
+	find_prime(x, 2);
+}

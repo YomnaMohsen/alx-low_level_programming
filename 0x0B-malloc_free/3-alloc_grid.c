34e4b221549e1879b70544b24798863db3f4634d
YomnaMohsen <ymn_mohsen@yahoo.com>
2023-10-04 12:40:41 -0700

    initialize 2d array with zeros

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,8 +14,17 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
 	for (i = 0; i < height; i++)
 		grid[i] = malloc(width * sizeof(int));
+	if (grid[i] == NULL)
+	{
+		for (; i >= 0; i--)
+			free(grid[i]);
+		free(grid);
+		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)

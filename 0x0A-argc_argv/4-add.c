ab0625d305f8fbc6abf0d845d70cea75fb70f96e
YomnaMohsen <ymn_mohsen@yahoo.com>
2023-10-06 12:24:41 -0700

    adding args of program

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -add poistive numbers  arguments
+ *@argc: number of command line arguments
+ *@argv: array of pointers to strings
+ *Return: 1 if fails else 0
+ */
+
+int main(int argc, char **argv)
+{
+	int x, y, result = 0;
+
+	if (argc == 1)
+	{
+		printf("0");
+		printf("\n");
+	}
+	for (x = 1; x < argc; x++)
+	{
+		for (y = 0; argv[x][y] != '\0'; y++)
+		{
+			if (argv[x][y] < '0' || argv[x][y] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		 result += atoi(argv[x]);
+	}
+	printf("%d", result);
+	printf("\n");
+	return (0);
+}

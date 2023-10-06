8e6b80157ec3db0c6cd99b1e107057bc7450b23c
YomnaMohsen <ymn_mohsen@yahoo.com>
2023-10-06 12:42:02 -0700

    adding args of program

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,24 +12,22 @@ int main(int argc, char *argv[])
 {
 	int x, y, result = 0;
 
-	if (argc == 1)
+	if (argc >  1)
 	{
-		printf("0");
-		printf("\n");
-	}
-	for (x = 1; x < argc; x++)
-	{
-		for (y = 0; argv[x][y] != '\0'; y++)
+		for (x = 1; x < argc; x++)
 		{
-			if (argv[x][y] < '0' || argv[x][y] > '9')
+			for (y = 0; argv[x][y] != '\0'; y++)
 			{
-				printf("Error\n");
-				return (1);
+				if (argv[x][y] < '0' || argv[x][y] > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
+			result += atoi(argv[x]);
 		}
-		 result += atoi(argv[x]);
+		printf("%d", result);
+		printf("\n");
+		return (0);
 	}
-	printf("%d", result);
-	printf("\n");
-	return (0);
 }

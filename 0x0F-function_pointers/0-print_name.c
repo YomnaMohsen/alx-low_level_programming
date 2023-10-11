320b8f8fb5c12089b2433375e3e4e55c9d1ef9a9
YomnaMohsen <ymn_mohsen@yahoo.com>
2023-10-11 12:46:53 -0700

    print name fn pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,3 @@
-#include "function_pointers.h"
-
 /**
  *print_name - print name
  *
@@ -10,7 +8,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL)
-		exit(EXIT_FAILURE);
-	(*f)(10);
+	if (name && f)
+		(*f)(name);
 }

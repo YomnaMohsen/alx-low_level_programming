10a6a24a11a84d22e58ded493c06bf6f115b5c60
YomnaMohsen <ymn_mohsen@yahoo.com>
2023-10-14 08:08:58 -0700

    print diff types of args

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,7 +14,7 @@ void print_all(const char *const format, ...)
 	char *s1;
 
 	va_start(ap, format);
-	while (format[i] != '\0')
+	while (format != NULL && format[i] != '\0')
 	{
 		switch (format[i])
 		{

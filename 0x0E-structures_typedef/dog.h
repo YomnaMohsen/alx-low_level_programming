5aeb748b89035b7d87186b007e8e3a21a9dd06a0
YomnaMohsen <ymn_mohsen@yahoo.com>
2023-10-18 03:48:46 -0700

    first struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+*struct dog - new type defines dog
+*@name: name of dog
+*@age: age of dog
+*@owner: ownwer name
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+

8f402d5b881e321ddb692aae123ac498c32a8e3f
YomnaMohsen <ymn_mohsen@yahoo.com>
2023-10-31 12:56:27 -0700

    operation selected by user

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,10 +17,9 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i;
 
-	for (i = 0; *(ops[i].op) != '\0'; i++)
+	while ((ops[i].op) != NULL  &&  *s != *(ops[i].op))
 	{
-		if (*s == *(ops[i].op))
-			return (ops[i].f);
+		i++;
 	}
 	return (ops[i].f);
 }

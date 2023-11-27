f9eccd56e471c016855e516766df0889c0fd93c2
YomnaMohsen <ymn_mohsen@yahoo.com>
2023-11-27 23:23:15 +0200

    update betty for 4th task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,18 +3,6 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include "main.h"
-
-/**
-*char *create_buf(char *buf)
-*{
-	buf = malloc(sizeof(char) * 1024);
-	if (!buf)
-	{
-		return (NULL);
-	}
-	return (buf);
-}
-*/
 /**
  * closefd - check if field descriptor closed normally or not
  * @fd: int field descriptor
@@ -32,12 +20,8 @@ void closefd(int fd)
  * main - copies content of  file to another file
  * @argc: number of args passed to main
  * @argv: array of pointers to args
- * Return:  0 on success 
+ * Return:  0 on success
  *
- * Description: if arguments number incorrect , exit code 97
- * 		if file_from does not exist or cannot read -exit code 98
- * 		if write in file_to fails or file cannot be created- exit code 99
- * 		if file_to or file_from cannot be closed -exit code 100 
  */
 int main(int argc, char *argv[])
 {
@@ -47,7 +31,7 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_no\n");
-		exit (97);
+		exit(97);
 	}
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
@@ -61,9 +45,9 @@ int main(int argc, char *argv[])
 		dprintf(2, "Error: Can't write to file %s\n", argv[2]);
 		exit(99);
 	}
-	while ((re = read(from, buf, 1024)) > 0)
+	while ((re = read(from, buf, 1024) > 0)
 	{
-		if ((write(to, buf, re) != re)) 
+		if (write(to, buf, re) != re)
 		{
 			dprintf(2, "Error: Can't write to file %s\n", argv[2]);
 			exit(99);
@@ -74,7 +58,6 @@ int main(int argc, char *argv[])
 			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-	
 	closefd(from);
 	closefd(to);
 	return (0);

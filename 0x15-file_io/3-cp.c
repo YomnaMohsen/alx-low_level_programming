9521af0b034911cb5512c8c082ecfca46ffe4a7d
YomnaMohsen <ymn_mohsen@yahoo.com>
2023-11-28 21:35:00 +0200

    update 4th task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_no\n");
+		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	from = open(argv[1], O_RDONLY);

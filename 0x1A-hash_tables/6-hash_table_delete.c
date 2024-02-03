2fe9b9cce2421a7040c8aaa48a0fc286a2a87711
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-02-04 00:38:33 +0200

    task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+/**
+ * 
+ * 
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+    unsigned long int i;
+	hash_node_t *curr, *tmp;
+
+    if (ht == NULL)
+    {
+        return;
+    }
+    for (i = 0; i < ht->size; i++)
+    {
+        if (ht->array[i])
+		{
+			curr = ht->array[i];
+			while (curr)
+			{
+                tmp = curr->next;
+                free(curr->key);
+                free(curr->value);
+                free(curr);
+                curr = tmp;
+            }
+        }
+    }
+    free(ht->array);
+    free(ht);
+}

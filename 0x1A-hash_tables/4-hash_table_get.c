55a1368695d55f40e526b0f465a80ae14cdd050a
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-02-03 23:05:39 +0200

    fix task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,6 +11,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *head;
 
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
 	index = key_index((unsigned char *)key, ht->size);
 	head = ht->array[index];
 	while (head)

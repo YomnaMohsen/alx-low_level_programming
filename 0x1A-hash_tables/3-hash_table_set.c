5100756511b1446bdf533c126df0780394a5bf15
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-02-05 22:37:15 +0200

    Task 6

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 #include <string.h>
 
-int find_key(hash_node_t *curr, const char *key, char *value);
+int find_key(hash_node_t *curr, const char *key, const char *value);
 /**
  * hash_table_set - add or update key value
  * @ht: hash table that you add or update pair to
@@ -13,22 +13,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *curr, *hnode;
-	char *val_c;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 	{
 		return (0);
 	}
 	index = key_index((unsigned char *)key, ht->size);
-	val_c = strdup(value);
-	if (val_c == NULL)
-	{
-		return (0);
-	}
 	curr = ht->array[index];/* pointer to null if empty or nodes*/
 	/*no key exists (whether there other nodes at same index)*/
 	/*or empty slot create node with key*/
-	if (find_key(curr, key, val_c) == 0)
+	if (find_key(curr, key, value) == 0)
 	{
 		hnode = malloc(sizeof(hash_node_t));
 		if (hnode == NULL)
@@ -61,14 +55,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
  * @value: string value
  * Return: 1 if found 0 if not found or fail to update
 */
-int find_key(hash_node_t *curr, const char *key, char *value)
+int find_key(hash_node_t *curr, const char *key, const char *value)
 {
 	while (curr)
 	{
 		if (strcmp(curr->key, key) == 0) /*update value of same key (key exists)*/
 		{
 			free(curr->value);
-			curr->value = value;
+			curr->value = strdup(value);
+			if (curr->value == NULL)
+			{
+				return (0);
+			}
 			return (1);
 		}
 		curr = curr->next;
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,33 +1,34 @@
 #include "hash_tables.h"
 #include <stdlib.h>
 /**
- * 
- * 
+ * hash_table_delete - delete entire hashtable
+ * @ht: pointr to hash table
+ * Return: Nothing
 */
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i;
+	unsigned long int i;
 	hash_node_t *curr, *tmp;
 
-    if (ht == NULL)
-    {
-        return;
-    }
-    for (i = 0; i < ht->size; i++)
-    {
-        if (ht->array[i])
+	if (ht == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
 		{
 			curr = ht->array[i];
 			while (curr)
 			{
-                free(curr->key);
-                free(curr->value);
-                tmp = curr->next;
-                free(curr);
-                curr = tmp;
-            }
-        }
-    }
-    free(ht->array);
-    free(ht);
+				tmp = curr->next;
+				free(curr->key);
+				free(curr->value);
+				free(curr);
+				curr = tmp;
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
 }

1dfbc5310d39bda904d8ce66a1f754b644753b5e
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-02-05 22:54:33 +0200

    fix task3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 #include <string.h>
 
-int find_key(hash_node_t *curr, const char *key, const char *value);
+int find_key(hash_node_t *curr, const char *key,  char *value);
 /**
  * hash_table_set - add or update key value
  * @ht: hash table that you add or update pair to
@@ -13,8 +13,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *curr, *hnode;
+	char *val;
 
 	if (ht == NULL || key == NULL || *key == '\0')
+	{
+		return (0);
+	}
+	val = strdup(value);
+	if (!val)
 	{
 		return (0);
 	}
@@ -22,26 +28,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	curr = ht->array[index];/* pointer to null if empty or nodes*/
 	/*no key exists (whether there other nodes at same index)*/
 	/*or empty slot create node with key*/
-	if (find_key(curr, key, value) == 0)
+	if (find_key(curr, key, val) == 0)
 	{
 		hnode = malloc(sizeof(hash_node_t));
 		if (hnode == NULL)
 		{
+			free(val);
 			return (0);
 		}
 		hnode->key = strdup(key);
 		if (hnode->key == NULL)
 		{
+			free(val);
 			free(hnode);
 			return (0);
 		}
-		hnode->value = strdup(value);
-		if (hnode->value == NULL)
-		{
-			free(hnode->key);
-			free(hnode);
-			return (0);
-		}
+		hnode->value = val;
 		/*collision ,add key at beginning or empty slot add new node*/
 		hnode->next = ht->array[index];
 		ht->array[index] = hnode;
@@ -55,18 +57,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
  * @value: string value
  * Return: 1 if found 0 if not found or fail to update
 */
-int find_key(hash_node_t *curr, const char *key, const char *value)
+int find_key(hash_node_t *curr, const char *key,  char *value)
 {
 	while (curr)
 	{
 		if (strcmp(curr->key, key) == 0) /*update value of same key (key exists)*/
 		{
 			free(curr->value);
-			curr->value = strdup(value);
-			if (curr->value == NULL)
-			{
-				return (0);
-			}
+			curr->value = value;
 			return (1);
 		}
 		curr = curr->next;
